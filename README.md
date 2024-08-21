98bd7667e0800bcf78e3043cc36bf747023c143a
Mario <mario@asaitec.eu>
2024-08-21 17:01:48 +0200

    editing readme

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# Getting Started with Create React App
+[![LinkedIn][linkedin-shield]][linkedin-url]
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+### Built With
+
+This section should list any major frameworks/libraries used to bootstrap your project. Leave any add-ons/plugins for the acknowledgements section. Here are a few examples.
+
+- [![React][React.js]][React-url]
+
+### About the project
+
+Just a list, made to get in touch for the first time with react State, and react components
+
+Credits to "The Ultimate React Course 2024: React, Next.js, Redux & More"
+Created by Jonas Schmedtmann
 
 ## Available Scripts
 
@@ -12,59 +23,11 @@ Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
 The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
 
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+[linkedin-url]: https://linkedin.com/in/mmasr
+[React.js]: https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB
+[React-url]: https://reactjs.org/
--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
   const [friends, setFriends] = useState(initialFriends);
   const [isAddFormOpen, setIsAddFormOpen] = useState(false);
   const [selectedFriend, setSelectedFriend] = useState(null);
-  
+
   function handleOpenAddForm() {
     setIsAddFormOpen((show) => !show);
   }
@@ -33,7 +33,7 @@ export default function App() {
   function handleAddFriend(newFriend) {
     console.log(friends);
     setFriends((curFriends) => [...curFriends, newFriend]);
-    console.log(friends)
+    console.log(friends);
   }
 
   function handleSelectFriend(chosen) {
@@ -41,62 +41,96 @@ export default function App() {
   }
 
   function handleSplitBill(value) {
-    setFriends((friends) => friends.map(friend => friend.id === selectedFriend.id ? {...friend, balance: friend.balance + value} : friend))
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
   }
-  
-  return (<div className="app">
-    <div className="sidebar">
-      <FriendList selectedFriend={selectedFriend} friends={friends} onSelectFriend={handleSelectFriend}/>
-      {isAddFormOpen && <FormAddFriend onAddFriend={handleAddFriend} />}
-      <Btn onClick={handleOpenAddForm}>{isAddFormOpen ? "Close" : "Add Friend"}</Btn>
 
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList
+          selectedFriend={selectedFriend}
+          friends={friends}
+          onSelectFriend={handleSelectFriend}
+        />
+        {isAddFormOpen && <FormAddFriend onAddFriend={handleAddFriend} />}
+        <Btn onClick={handleOpenAddForm}>
+          {isAddFormOpen ? "Close" : "Add Friend"}
+        </Btn>
+      </div>
+      {selectedFriend && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSplitBill={handleSplitBill}
+        />
+      )}
     </div>
-    {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill} />}
-  </div>);
+  );
 }
 
 function Btn({ children, onClick }) {
-
-  return (<button className="button" onClick={onClick}>{children}</button>);
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
 function FriendList({ friends, onSelectFriend, selectedFriend }) {
-  
-  return (<ul>
-    {friends.map(friend => <Friend selectedFriend={selectedFriend} friend={friend} key={friend.id} onSelectFriend={onSelectFriend} />)}
-  </ul>);
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          selectedFriend={selectedFriend}
+          friend={friend}
+          key={friend.id}
+          onSelectFriend={onSelectFriend}
+        />
+      ))}
+    </ul>
+  );
 }
 
-function Friend({friend, onSelectFriend, selectedFriend}) {
+function Friend({ friend, onSelectFriend, selectedFriend }) {
   const isSelected = selectedFriend?.id === friend.id;
 
-  return (<li className={isSelected ? "selected" : ""}>
-    <img src={friend.image} alt={friend.name} />
-    <h3>{friend.name}</h3>
-    
-    {friend.balance < 0 && (
-      <p className="red">We owe this light creature {friend.name} {Math.abs(friend.balance)}€</p>
-    )}
-    {friend.balance > 0 && (
-      <p className="green">This little bitch owe's us {friend.name} {Math.abs(friend.balance)}€</p>
-    )}
-    {friend.balance === 0 && (
-      <p>We don't work with pleasants like {friend.name}</p>
-    )}
+  return (
+    <li className={isSelected ? "selected" : ""}>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
 
-    <Btn onClick={() => onSelectFriend(friend)}>Select</Btn>
-  </li>);
+      {friend.balance < 0 && (
+        <p className="red">
+          We owe this light creature {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          This little *** owe's us {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance === 0 && (
+        <p>We don't work with pleasants like {friend.name}</p>
+      )}
+
+      <Btn onClick={() => onSelectFriend(friend)}>Select</Btn>
+    </li>
+  );
 }
 
-
-function FormAddFriend({onAddFriend}) {
+function FormAddFriend({ onAddFriend }) {
   const [name, setName] = useState("");
   const [image, setImage] = useState("https://i.pravatar.cc/48");
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    if(!name || !image) return;
+    if (!name || !image) return;
 
     const id = crypto.randomUUID();
     const newFriend = {
@@ -111,54 +145,71 @@ function FormAddFriend({onAddFriend}) {
     setName("");
     setImage("https://i.pravatar.cc/48");
   }
-  
-  return (<form className="form-add-friend" onSubmit={handleSubmit}>
-    <label>👼Friend</label>
-    <input type="text" value={name} onChange={(e) => setName((curName) => e.target.value)}/>
 
-    <label>⏹Image URL</label>
-    <input type="text" value={image} onChange={(e) => setImage((curImg) => e.target.value)}/>
+  return (
+    <form className="form-add-friend" onSubmit={handleSubmit}>
+      <label>👼Friend</label>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName((curName) => e.target.value)}
+      />
 
-    <Btn>Add</Btn>
-  </form>);
+      <label>⏹Image URL</label>
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage((curImg) => e.target.value)}
+      />
+
+      <Btn>Add</Btn>
+    </form>
+  );
 }
 
-function FormSplitBill({selectedFriend, onSplitBill}) {
-const [bill, setBill] = useState("");
-const [paidByUser, setPaidByUser] = useState("");
-const [who, setWho] = useState("");
-var paidBySlave = 0;
-paidBySlave = paidByUser <= bill ?  (paidBySlave = bill - paidByUser) : 0;
+function FormSplitBill({ selectedFriend, onSplitBill }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const [who, setWho] = useState("");
+  var paidBySlave = 0;
+  paidBySlave = paidByUser <= bill ? (paidBySlave = bill - paidByUser) : 0;
 
-function handleSubmit(e) {
-  e.preventDefault();
+  function handleSubmit(e) {
+    e.preventDefault();
 
-  if(!bill || !paidByUser || paidByUser > bill) return;
+    if (!bill || !paidByUser || paidByUser > bill) return;
 
-  onSplitBill(who === "user" ? paidByUser : -paidBySlave);
+    onSplitBill(who === "user" ? paidByUser : -paidBySlave);
+  }
+
+  return (
+    <form className="form-split-bill" onSubmit={handleSubmit}>
+      <h2>Split a bill with {selectedFriend.name}</h2>
+
+      <label>🤬 Bill Value</label>
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
+
+      <label>🤦‍♀️ Your expense</label>
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) => setPaidByUser(Number(e.target.value))}
+      />
+
+      <label>🤳 {selectedFriend.name} expense</label>
+      <input type="text" value={paidBySlave} disabled />
+
+      <label>🤑 Who's paying the bill?</label>
+      <select value={who} onChange={(e) => setWho(e.target.value)}>
+        <option value="user">You (Ha!)</option>
+        <option value="friend">{selectedFriend.name}</option>
+      </select>
+
+      <Btn>Split Bill</Btn>
+    </form>
+  );
 }
-
-return (<form className="form-split-bill" onSubmit={handleSubmit}>
-    <h2>Split a bill with {selectedFriend.name}</h2>
-
-    <label>🤬 Bill Value</label>
-    <input type="text" value={bill} onChange={(e) => setBill(Number(e.target.value))} />
-
-    <label>🤦‍♀️ Your expense</label>
-    <input type="text"  value={paidByUser} onChange={(e) => setPaidByUser(Number(e.target.value))} />
-
-    <label>🤳 {selectedFriend.name} expense</label>
-    <input type="text" value={paidBySlave} disabled/>
-
-    <label>🤑 Who's paying the bill?</label>
-    <select value={who} onChange={(e) => setWho(e.target.value)}>
-      <option value="user">You (Ha!)</option>
-      <option value="friend">{selectedFriend.name}</option>
-    </select>
-
-    <Btn>Split Bill</Btn>
-  </form>);
-}
-
-
-
